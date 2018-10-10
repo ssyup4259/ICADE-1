767973fb023e5cd7fc8827e227fee834d7d20a25
hypochratesm <hypochrates@naver.com>
2018-10-10 17:46:19 +0900

    삭제 수정

--- src/main/java/com/project/controller/MemberController.java
+++ src/main/java/com/project/controller/MemberController.java
@@ -1,7 +1,6 @@
 package com.project.controller;
 
 
-import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -13,56 +12,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.project.dao.MemberDAO;
 import com.project.dto.MemberDTO;
 import com.project.service.MemberService;
-
-
 @Controller
 public class MemberController {
 
-	@Autowired
-	MemberDAO m_dao;
-	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
-	@Autowired
-	private MemberService service; 
-	
-	@RequestMapping(value="/join.action", method= {RequestMethod.GET, RequestMethod.POST})
-	public String join(HttpServletRequest req, HttpServletResponse resp) {
-		System.out.println("aaa");
-		
-		return "member/join";
-	}
-	
-	@RequestMapping(value="/join_ok.action", method= {RequestMethod.GET,RequestMethod.POST})
-	public String join_ok(MemberDTO dto, HttpServletRequest req, HttpServletResponse resp)throws Exception{
-		
-		m_dao.insertData(dto);
-		
-		
-		return "redirect:/list.action";
-	}
-	@RequestMapping(value="/emailcheck",method= RequestMethod.POST)
-	public String emailCheck(MemberDTO dto, Model model, RedirectAttributes rttr, HttpServletRequest req, HttpSession session)throws Exception{
-		
-		logger.info("회원가입...");
-		logger.info(dto.toString());
-		service.insertData(dto);
-		rttr.addFlashAttribute("authmsg" , "가입시 사용한 이메일로 인증해주세요");
-		return "redirect:/";
-		
-		
-	}
-	@RequestMapping(value = "/emailConfirm", method = RequestMethod.GET)
-	public String emailConfirm(String mEmailId, Model model) throws Exception { // 이메일인증
-		service.userAuth(mEmailId);
-		model.addAttribute("user_email", mEmailId);
-
-		return "/user/emailConfirm";
-	}
 
 
 }
--- src/main/java/com/project/service/MemberService.java
+++ src/main/java/com/project/service/MemberService.java
@@ -18,32 +18,5 @@ import com.project.service.TempKey;
 
 @Repository
 public class MemberService {
-	@Inject
-	private JavaMailSender mailSender;
-	private SqlSessionTemplate sessionTemplate;
 
-	public void setSessionTemplate(SqlSessionTemplate sessionTemplate) throws SQLException {
-		this.sessionTemplate = sessionTemplate;
-	}
-	@Transactional
-	public void insertData(MemberDTO dto) throws Exception {
-		sessionTemplate.insert("joinMapper.insertData", dto);
-		
-		String key = new TempKey().getKey(50,false);
-		
-		sessionTemplate.update("joinMapper.createAuthkey", key);
-		
-		MailHandler sendMail = new MailHandler(mailSender);
-		
-		sendMail.setSubject("[이메일 인증]");
-		sendMail.setText(new StringBuffer().append("<h1>메일인증</h1>").append("<a href='http://192.168.16.19:8080/icade/emailConfirm?user_email=").append(dto.getMEmailId()).append("&key=").append(key).append("' target='_blenk'>이메일 인증 확인</a>").toString());
-		sendMail.setFrom("호스트 이메일 아이디", "관리자");
-		sendMail.setTo(dto.getMEmailId());
-		sendMail.send();
-		
-	}
-	public void userAuth(String mEmailId) throws Exception {
-		sessionTemplate.update(mEmailId);
-	}
-	
 }
