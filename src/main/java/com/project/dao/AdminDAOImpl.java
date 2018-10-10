d67ca6c4153c37612aec43dde49878c82c25310e
박명성 <mspark0322@gmail.com>
2018-10-10 15:54:13 +0900

    상품 등록 jsp 파일 생성

--- src/main/java/com/project/dao/AdminDAOImpl.java
+++ src/main/java/com/project/dao/AdminDAOImpl.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.project.dto.GoodsDTO;
@@ -13,6 +14,7 @@ import com.project.dto.OrdersDTO;
 @Repository
 public class AdminDAOImpl implements AdminDAO {
 	
+	@Autowired
 	private SqlSessionTemplate sessionTemplate;
 
 	public void setSessionTemplate(SqlSessionTemplate sessionTemplate) throws SQLException {
@@ -22,38 +24,44 @@ public class AdminDAOImpl implements AdminDAO {
 	public static String namespace = "com.project.mybatis.adminMapper";
 
 	@Override
-	public void insertGoods(GoodsDTO g_dto) {
+	public void insertGoods(GoodsDTO g_dto) throws Exception  {
 		
 		sessionTemplate.insert(namespace + ".insertGoods", g_dto);
 		
 	}
 
 	@Override
-	public void updateGoods(GoodsDTO g_dto) {
+	public void updateGoods(GoodsDTO g_dto) throws Exception  {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void deleteGoods(String g_code) {
+	public void deleteGoods(String g_code) throws Exception  {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public List<MemberDTO> memberList() {
+	public List<MemberDTO> memberList() throws Exception  {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void authority(String m_id) {
+	public void authority(String m_id) throws Exception  {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public List<OrdersDTO> ordersList() {
+	public List<OrdersDTO> ordersList() throws Exception  {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<GoodsDTO> goodsList() throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- src/main/java/com/project/service/AdminService.java
+++ src/main/java/com/project/service/AdminService.java
@@ -9,16 +9,18 @@ import com.project.dto.OrdersDTO;
 public interface AdminService {
 	
 	//상품 등록
-	public void insertGoods(GoodsDTO g_dto);
+	public void insertGoods(GoodsDTO g_dto) throws Exception;
 	//상품 수정(재고 수정)
-	public void updateGoods(GoodsDTO g_dto);
+	public void updateGoods(GoodsDTO g_dto) throws Exception;
 	//상품 삭제
-	public void deleteGoods(String g_code);
+	public void deleteGoods(String g_code) throws Exception;
+	//상품 리스트
+	public List<GoodsDTO> goodsList() throws Exception;
 	//회원 리스트
-	public List<MemberDTO> memberList();
+	public List<MemberDTO> memberList() throws Exception;
 	//회원에게 권한 부여
-	public void	authority(String m_id);
+	public void	authority(String m_id) throws Exception;
 	//회원 주문내역 조회
-	public List<OrdersDTO> ordersList();
+	public List<OrdersDTO> ordersList() throws Exception;
 
 }
