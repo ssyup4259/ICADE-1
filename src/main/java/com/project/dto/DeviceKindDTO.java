4b700d34051a3615e0727296d810bad23e0ecb2f
박명성 <mspark0322@gmail.com>
2018-10-11 17:56:00 +0900

    상품등록 view 수정중

--- src/main/java/com/project/dto/DeviceKindDTO.java
+++ src/main/java/com/project/dto/DeviceKindDTO.java
@@ -1,5 +1,27 @@
 package com.project.dto;
 
 public class DeviceKindDTO {
+	
+	// 기종 코드 
+    private int DK_CODE;
 
+    // 핸드폰 기종 
+    private String DK_NAME;
+
+	public int getDK_CODE() {
+		return DK_CODE;
+	}
+
+	public void setDK_CODE(int dK_CODE) {
+		DK_CODE = dK_CODE;
+	}
+
+	public String getDK_NAME() {
+		return DK_NAME;
+	}
+
+	public void setDK_NAME(String dK_NAME) {
+		DK_NAME = dK_NAME;
+	}
+    
 }
