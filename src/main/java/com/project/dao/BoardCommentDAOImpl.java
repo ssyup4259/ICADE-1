47519a49b0d7be6a8fca66f97c474c5360970228
hypochratesm <hypochrates@naver.com>
2018-10-25 12:05:50 +0900

    댓글 테이블 수정/ 댓글 기능 50%

--- src/main/java/com/project/dao/BoardCommentDAOImpl.java
+++ src/main/java/com/project/dao/BoardCommentDAOImpl.java
@@ -4,13 +4,11 @@ import java.io.File;
 import java.sql.SQLException;
 import java.util.List;
 
-import org.apache.ibatis.session.SqlSession;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.project.dto.BoardCommentDTO;
-import com.project.dto.GoodsDTO;
 
 @Repository
 public class BoardCommentDAOImpl implements BoardCommentDAO {
@@ -25,6 +23,8 @@ public class BoardCommentDAOImpl implements BoardCommentDAO {
 	
 	public static String replyMapper = "com.project.mybatis.replyMapper";
 	
+	
+	//댓글입력
 	@Override
 	public int insertData(BoardCommentDTO bc_dto) throws Exception {
 		
@@ -39,19 +39,28 @@ public class BoardCommentDAOImpl implements BoardCommentDAO {
 		return bc_dto.getBC_NUM();
 		
 	}
-
+	
+	//댓글리스트
 	@Override
-	public List<BoardCommentDTO> getReadData(String BC_ID) throws Exception {
+	public List<BoardCommentDTO> replyList(String BC_ID) throws Exception {
 		
 		return sessionTemplate.selectList(replyMapper + ".listData");
 	}
+	
+	//하나의 댓글 읽어오기
+	@Override
+	public BoardCommentDTO getReadData(int bc_num) throws Exception {
+		return sessionTemplate.selectOne(replyMapper +".readData");
+	}
 
+	
+	//댓글 수정
 	@Override
 	public void updateData(BoardCommentDTO bc_dto) throws Exception {
 		
 		sessionTemplate.update(replyMapper + ".updateData", bc_dto);
 	}
-
+	//댓글 삭제
 	@Override
 	public void deleteData(int BC_NUM,String path) throws Exception {
 		
@@ -67,5 +76,8 @@ public class BoardCommentDAOImpl implements BoardCommentDAO {
 		}
 		sessionTemplate.delete(replyMapper + ".deleteGoods", BC_NUM);
 	}
+
+
+
 
 }
