a749977e9ad84989ec7684f395516cd05475c200
hypochratesm <hypochrates@naver.com>
2018-10-25 09:48:47 +0900

    edddd

--- src/main/java/com/project/controller/BoardCommentController.java
+++ src/main/java/com/project/controller/BoardCommentController.java
@@ -1,8 +1,16 @@
 package com.project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+import com.project.service.BoardCommentService;
 
+@Controller
 public class BoardCommentController {
+	
+	
+	@Autowired
+	BoardCommentService bc_service;
 	
 	
 
